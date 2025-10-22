0922447d66998a8c9b9cf3ca0bf653050d1fc653
chocycat <chocycat@catboy.to>
2025-10-22 02:24:49 +0200

    feat: typescript LSP recipe

---
+++ recipes/typescript_language_server.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+Elspy.recipe 'typescript_language_server' do
+  depends_on any: %w[npm pnpm]
+
+  binary 'node_modules/.bin/typescript-language-server'
+
+  download do |version: :latest|
+    node_install 'typescript-language-server', version
+  end
+
+  run do |args|
+    exec binary_path, '--stdio', *args
+  end
+end
